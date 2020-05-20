34253a4b028b15b87b539420c4dd1975589d0445
semyonkungurov <s.kungurov8679@mail.ru>
2020-05-20 17:35:44 +0700

    Changed start

--- start.py
+++ start.py
@@ -1,21 +1,21 @@
 from menu import Menu
 
 
-def open_file(text): # открываем текстовый документ
+def open_file(text): # открываем как текстовый документ
     try:
         file = open(text, encoding='utf-8')
-        file.close()
+        file.close() #  закрываем
         return True
     except IOError:
         return False
 def main(): # работаем с текстовым документом
-    text = '1.txt'
-    check = open_file(text)
+    text = '1.txt' # присваиваем переменной документ
+    check = open_file(text) # открываем как текстовый документ
     if check is True:
-        with open('1.txt', encoding='utf-8') as f:
+        with open('1.txt', encoding='utf-8') as f: # читаем текстовый документ
             strokes = f.readlines()
-        lines = [line.strip() for line in strokes]
-        f.close()
+        lines = [line.strip() for line in strokes] # делим весь текст на строки
+        f.close() # закрываем текстовый документ
         app = Menu(lines)
         app.mainloop()
     else:
