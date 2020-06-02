8200cac9ad0a5e408a825020a7b1d96c6c21854e
semyonkungurov <s.kungurov8679@mail.ru>
2020-06-02 21:00:20 +0700

    Changed launch.py

--- src/launch.py
+++ src/launch.py
@@ -1,11 +1,13 @@
+"""Start program"""
 from menu import Menu
 
 
-def main(): # работаем с текстойвым документом
-    text=open('../1.txt', encoding='utf-8')
-    list = text.readlines()
-    lines = [f.strip() for f in list]
+def main():
+    """работаем с текстойвым документом"""
+    text = open('../1.txt', encoding='utf-8')
+    dictionary = text.readlines()
+    lines = [f.strip() for f in dictionary]
     text.close()
-    menu=Menu(lines)
+    menu = Menu(lines)
     menu.mainloop()
 main()
