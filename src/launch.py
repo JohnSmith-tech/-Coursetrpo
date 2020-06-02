f4742a8b1844292f88e142d10a9976ef3cb71bca
semyonkungurov <s.kungurov8679@mail.ru>
2020-06-02 20:36:23 +0700

    Changed launch.py

--- src/launch.py
+++ src/launch.py
@@ -2,7 +2,7 @@ from menu import Menu
 
 
 def main(): # работаем с текстойвым документом
-    text=open('1.txt', encoding='utf-8')
+    text=open('../1.txt', encoding='utf-8')
     list = text.readlines()
     lines = [f.strip() for f in list]
     text.close()
