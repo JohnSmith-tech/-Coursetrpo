8d56f043ede784fb889015d59c7d5c9e13c48a2b
Eugeny Ilyin <iea2001@mail.ru>
2020-06-12 21:11:40 +0700

    Add new tests

--- test/tests.py
+++ test/tests.py
@@ -1,5 +1,6 @@
 import unittest
-from program import error
+from src.program import error
+from src.program import checking
 
 class TestUnits(unittest.TestCase):
     def test_blocked_1(self):
@@ -9,6 +10,7 @@ class TestUnits(unittest.TestCase):
     def test_blocked_2(self):
         bl_2 = error('police')
         self.assertEqual(True, bl_2)
+
     def test_blocked_3(self):
         bl_3 = error('pol1ce')
         self.assertEqual(False, bl_3)
@@ -21,7 +23,31 @@ class TestUnits(unittest.TestCase):
         bl_5 = error('213112')
         self.assertEqual(False, bl_5)
 
+    def test_check1(self):
+        ch_1 = checking('drive12r', ['водитель', 'driver'])
+        self.assertEqual(False, ch_1)
+
+    def test_check2(self):
+        ch_2 = checking('', ['водитель', 'driver'])
+        self.assertEqual(False, ch_2)
+
+    def test_check3(self):
+        ch_3 = checking(' ', ['полицейский', 'police'])
+        self.assertEqual(False, ch_3)
+
+    def test_check4(self):
+        ch_4 = checking(' police', ['полицейский', 'police'])
+        self.assertEqual(False, ch_4)
+
+    def test_check5(self):
+        ch_5 = checking('driver', ['водитель', 'driver'])
+        self.assertEqual(True, ch_5)
+
+    def test_check6(self):
+        ch_6 = checking('police', ['полицейский', 'police'])
+        self.assertEqual(True, ch_6)
 
 if __name__ == '__main__':
     unittest.main()
+
 
